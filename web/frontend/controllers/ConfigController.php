8e5bcf7a246265b5be70f1503c69db9be6d632de
JAY <JAY@oonne.com>
2020-07-29 21:04:42 +0800

    config

---
+++ web/frontend/controllers/ConfigController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Response;
+use yii\data\ActiveDataProvider;
+use frontend\filters\AuthFilter;
+use frontend\helpers\exportMsg;
+use common\models\Config;
+
+class ConfigController extends Controller
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => AuthFilter::className()
+        ];
+        return $behaviors;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    protected function verbs()
+    {
+        return [
+            'update' => ['post'],
+        ];
+    }
+
+    public function actionUpdate()
+    {
+        $config = Yii::$app->request->post();
+        $key = $config['sConfigKey'];
+        $model = Config::find()
+                    ->where(['sConfigKey' => $key])
+                    ->one();
+
+        if (!$model) {
+            return exportMsg::error('101000');
+        }
+
+        if ($model->load($config, '') && $model->validate()) {
+            $model->uLastAccountID = Yii::$app->user->id;
+            if ($model->save(false)) {
+                return exportMsg::ok();
+            } else {
+                return exportMsg::error('101003');
+            }
+        }
+
+        return [
+            'Ret' => '101003',
+            'Data' => [
+                'errors' => $model->getFirstErrors()
+            ]
+        ];
+    }
+}
