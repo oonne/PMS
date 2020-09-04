62efdce4f3d7b5c3b2cb19f93a6d64015f435c97
JAY <JAY@oonne.com>
2020-09-04 16:29:08 +0800

    登录功能完成

--- quick-app/src/Common/api/request.js
+++ quick-app/src/Common/api/request.js
@@ -3,12 +3,25 @@ import storage from '@system.storage'
 import config from '../config/config'
 
 const getToken = () => {
-  return new Promise(resolve, reject) => {
+  return new Promise((resolve, reject) => {
     storage.get({
       key: 'token',
       default: '',
       success: (data) => {
         resolve(data)
+      },
+      fail: (e) => {
+        reject(e)
+      },
+    })
+  })
+}
+const deleteToken = () => {
+  return new Promise((resolve, reject) => {
+    storage.delete({
+      key: 'token',
+      success: () => {
+        resolve()
       },
       fail: (e) => {
         reject(e)
@@ -24,11 +37,20 @@ const request = (params) => {
       method: params.method,
       header: {
         'x-auth-token': token,
+        'content-type': 'application/json',
       },
       responseType: 'json',
       data: params.data,
+    }).then(res=>{
+      if (res.data.code == 200) {
+        return res.data.data
+      } else if (res.data.code == 401) {
+        return deleteToken()
+      } else {
+        return Promise.reject(res)
+      }
     })
   })
-})
+}
 
 export default request
