b14d649f13439e96aa5e0b6694b7d3f3026be1d0
JAY <JAY@oonne.com>
2020-09-07 20:04:22 +0800

    计算计步器的每周、每月、总平均值

--- quick-app/src/Common/api/index.js
+++ quick-app/src/Common/api/index.js
@@ -10,14 +10,21 @@ export default {
     })
   },
   /* 刷新最后登录时间 */
-  safetyRefresh(data){
+  safetyRefresh(){
     return request({
       method: 'POST',
       url: '/safety/refresh',
-      data: data
+      data: null
     })
   },
   /* 计步器 */
+  pedometerIndex(){
+    return request({
+      method: 'GET',
+      url: '/pedometer/index',
+      data: null
+    })
+  },
   pedometerUpdate(data){
     return request({
       method: 'POST',
