49a89db311afba74457f7511aeb14e9cfb066366
JAY <hejiayi@kp99.cn>
2024-03-07 09:59:53 +0800

    feat: 修改语言

--- chrome/newtab/index.js
+++ chrome/newtab/index.js
@@ -12,7 +12,7 @@ const refreshTime = () => {
   let now = new Date();
   let clock = `${paddedNum(now.getHours())}:${paddedNum(now.getMinutes())}:${paddedNum(now.getSeconds())}`;
   $('#clock').html(clock);
-  let day = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日',][now.getDay()];
+  let day = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日',][now.getDay()];
   let date = `${now.getFullYear()}年${paddedNum(now.getMonth()+1)}月${paddedNum(now.getDate())}日`;
   $('#calendar').html(`${date} ${day}`);
 }
